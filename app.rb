909744a1c9bbe0b6377995c14ef7e0f2d6a8d38d
silvanagv <silvanagoberdhanvigle@gmail.com>
2016-08-11 21:26:25 -0400

    finished

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    @team_info = params
+    erb :team
 
+    end
+
+  get '/team' do
+
+  @team_info
+    erb :team
+
+  end
 end
